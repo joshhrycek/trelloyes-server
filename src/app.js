cbcb86851f19b339966b52c765f22aa98a4e8f5c
Hrycek <HrycekJ@pennmedicine.upenn.edu>
2019-09-29 14:58:39 -0400

    routing complete

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const uuid = require('uuid/v4')
+const cardRouter = require('./card/card-router');
+const listRouter = require('./list/list-router')
 
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -14,6 +17,21 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+  app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      logger.error(`Unauthorized request to path: ${req.path}`);
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+  })
+
+app.use(cardRouter)
+app.use(listRouter)
 
 app.get('/', (req,res) => {
     res.send('Hello, boilerplate!')
